fe327a0f0ba1803c973deb40cf51c3894caa655b
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:43:56 +0100

    done

--- main.sql
+++ main.sql
@@ -1,6 +1,9 @@
 CREATE TABLE cities (name TEXT, country TEXT);
 INSERT INTO cities VALUES ('Tokyo', 'Japan');
 INSERT INTO cities VALUES ('Paris', 'France');
+INSERT INTO cities VALUES ('Osaka', 'Japan');
+INSERT INTO cities VALUES ('Kyoto', 'Japan');
 INSERT INTO cities VALUES ('Cairo', 'Egypt');
 
-SELECT name FROM cities;
+-- SELECT name FROM cities WHERE ...
+SELECT name FROM cities WHERE country = 'Japan';
