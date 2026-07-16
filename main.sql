8ba192842799b15aebe1023d81507bb95d3acab8
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:42:27 +0100

    done

--- main.sql
+++ main.sql
@@ -1,1 +1,6 @@
-SELECT 'Hello,' || ' SQL!' ;
+CREATE TABLE cities (name TEXT, country TEXT);
+INSERT INTO cities VALUES ('Tokyo', 'Japan');
+INSERT INTO cities VALUES ('Paris', 'France');
+INSERT INTO cities VALUES ('Cairo', 'Egypt');
+
+SELECT name;
