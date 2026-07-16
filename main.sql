7c13c6a861606eccb8962333dede1c3ddc4d4f06
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-16 18:56:34 +0100

    done

--- main.sql
+++ main.sql
@@ -8,6 +8,6 @@ INSERT INTO employees VALUES
 -- TODO: UPDATE Bob's salary to 90000
 UPDATE employees SET salary = 90000 WHERE name = 'Bob';
 -- TODO: DELETE Carol
-DELETE FROM employees WHERE Name = 'Carol'
+DELETE FROM employees WHERE Name = 'Carol';
 -- TODO: SELECT name || '|' || salary ORDER BY name
 SELECT name, salary FROM employees ORDER BY name;
